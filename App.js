898e1b2168f442fc5a3e4004cf8811892d036c4c
Jared Alvidrez <a1121250005@utch.edu.mx>
2022-03-11 23:30:13 -0700

    Actividad 8 version 2  (minimal)

--- App.js
+++ App.js
@@ -1,13 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [datos, setDatos] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+    const getPosts = async() => {
+      try{
+        const url = "https://jsonplaceholder.typicode.com/posts";
+
+        //Consumir los datos 
+        const response = await fetch(url);
+        //convertir a json
+        const json = await response.json();
+        setDatos(json);
+      } catch (error){
+        console.error(error);
+      } finally {
+        setLoading(false)
+      }
+      
+}
+
+useEffect(() => {
+  getPosts();
+}, [])
+
+return(
+  <View style={styles.container}>
+    {isLoading ? <ActivityIndicator/> : (
+    <FlatList
+    data={datos}
+    keyExtractor = {( {id}, index) => id}
+    renderItem = {
+      ({item}) => (
+        <Text>{item.title}</Text>
+      )
+    }
+    />
+    )}
+  </View>
+);
 }
 
 const styles = StyleSheet.create({
